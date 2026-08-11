4bc6a3dc5e33324b882d8ce2d6c6e052def8ecdc
jack345678 <75750939+jack345678@users.noreply.github.com>
2026-08-11 08:59:29 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ServicesGrid from "./components/ServicesGrid";
 import WhyChooseUs from "./components/WhyChooseUs";
@@ -6,20 +5,3 @@ import Technologies from "./components/Technologies";
 import Industries from "./components/Industries";
 import Testimonials from "./components/Testimonials";
 import CTA from "./components/CTA";
-import Footer from "./components/Footer";
-
-export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <ServicesGrid />
-      <WhyChooseUs />
-      <Technologies />
-      <Industries />
-      <Testimonials />
-      <CTA />
-      <Footer />
-    </>
-  );
-}
